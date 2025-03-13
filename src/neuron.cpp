c7450bfecc3a3e572568ebc43376c585fb97b8a9
sourcespring <entradox.ftc@gmail.com>
2025-03-12 23:41:27 -0400

    Update neuron.cpp

--- src/neuron.cpp
+++ src/neuron.cpp
@@ -1,21 +1,32 @@
 #include "neuron.h"
 #include "synapse.h"
 #include <iostream>
+#include <vector>
 
-Neuron::Neuron(int id) : id(id), activation(0.0) {}
+Neuron::Neuron(int id) : id(id), activation(0.0), isFiring(false) {
+    synapses.reserve(10); 
+}
 
 void Neuron::receiveSignal(double signal) {
     activation += signal;
+    if (activation > 1.0) {  
+        isFiring = true;
+    }
 }
 
 void Neuron::update() {
-    if (activation > 1.0) {
-        std::cout << "Neuron " << id << " fired!\n";
-        activation = 0.0;
-        
-        for (auto synapse : synapses) {
-            synapse->propagate();
-        }
+    if (!isFiring) return;  
+
+    
+#ifdef DEBUG
+    std::cout << "Neuron " << id << " fired!\n";
+#endif
+
+    activation = 0.0;
+    isFiring = false; 
+
+    for (Synapse* synapse : synapses) {
+        synapse->propagate();
     }
 }
 
